58c96a03b673e7d76e64675744245adff35691bc
tongoonamujera <tongoonamujera@gmail.com>
2021-06-26 12:16:51 +0200

    added second test to my_each

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,10 +1,14 @@
 require_relative "../enumerables"
 
 describe '#my_each' do
-  it 'should execute each element in an array when block is given' do
     range = Array(1..6)
     array = []
+  it 'should execute each element in an array when block is given' do
     range.my_each { |x| array << x }
     expect(array).to eq(range)
+  end
+
+  it 'should return an enumerator if no block is given' do
+    expect(range.my_each).to be_a(Enumerator)
   end
 end
