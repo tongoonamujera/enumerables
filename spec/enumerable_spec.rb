8795818686c68720fa85fad08fc48cc12c2a521d
tongoonamujera <tongoonamujera@gmail.com>
2021-06-26 20:41:51 +0200

    added some negative tests

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -4,9 +4,16 @@ describe '#my_each' do
     range = Array(1..6)
     array = []
 
+    arr = (1...6)
+
   it 'should execute each element in an array when block is given' do
     range.my_each { |x| array << x }
     expect(array).to eq(range)
+  end
+
+  it 'should execute each element in an array when block is given' do
+    range.my_each { |x| array << x }
+    expect(array).not_to eq(arr)
   end
 
   it 'should return an enumerator if no block is given' do
@@ -37,6 +44,10 @@ describe '#my_select' do
     expect(array).to eq([1, 3, 5])
   end
 
+  it 'should return an empty array if no matching condition in the block when block is given' do
+    expect(range.my_select { |x| x > 7 }).to eq([])
+  end
+
   it 'should return an enumerator if no block is given' do
     expect(range.my_select).to be_a(Enumerator)
   end
@@ -52,6 +63,10 @@ describe '#my_none?' do
     expect(range.my_none? { |x| x > 8 }).to eq(true)
   end
 
+  it 'should return false if an array contains a matching  condition to the block, when block is given' do
+    expect(range.my_none? { |x| x.odd? }).to eq(false)
+  end
+
   it 'should return an enumerator if no block is given' do
     expect(range.my_none?).to be_a(Enumerator)
   end
@@ -65,6 +80,14 @@ describe '#my_all?' do
 
   it 'should return true if  condition met in the block, when block is given' do
     expect(range.my_all? { |x| x < 8 }).to eq(true)
+  end
+
+  it 'should return true if  condition met in the block, when block is given' do
+    expect(range.my_all? { |x| x <= 6 }).to eq(true)
+  end
+
+  it 'should return false if no  condition met in the block, when block is given' do
+    expect(range.my_all? { |x| x >= 6 }).to eq(false)
   end
 
   it 'should return false if  condition is not met in the block, when block is given' do
@@ -92,6 +115,10 @@ describe '#my_any?' do
     expect(range.my_any? { |x| x.is_a?(String) }).to eq(false)
   end
 
+  it 'should return true if any element in an array matches the condition in the block, when block is given' do
+    expect(range.my_any? { |x| x.is_a?(Integer) }).to eq(true)
+  end
+
   it 'should return an enumerator if no block is given' do
     expect(range.my_any?).to be_a(Enumerator)
   end
@@ -113,6 +140,10 @@ describe '#my_count' do
     expect(range.my_count { |x| x.is_a?(String) }).to eq(0)
   end
 
+  it 'should return the number of element(s) in an array matching the condition in the block, when block is given' do
+    expect(array.my_count { |x| x > 3 }).to eq(4)
+  end
+
   it 'should return an enumerator if no block is given' do
     expect(range.my_count).to be_a(Enumerator)
   end
@@ -123,6 +154,10 @@ describe '#my_map' do
 
   it 'should return a new array when basing on the condition of the given block' do
     expect(range.my_map { |x| x * 2 }).to eq([2, 4, 6, 8, 10, 12])
+  end
+
+  it 'should return a new array when basing on the condition of the given block' do
+    expect(range.my_map { |x| x * 2 }).not_to eq([2, 4, 6, 8, 10, 12, 14])
   end
 
   it 'should return an enumerator if no block is given' do
@@ -137,7 +172,7 @@ describe '#my_inject' do
     expect(range.my_inject(:+)).to eq(21)
   end
 
-  it ' should return sum of all elements in an array starting from parameter given' do
+  it 'should return sum of all elements in an array starting from parameter given' do
     expect(range.my_inject(5) { |sum, value| sum + value }).to eq(26)
   end
 
